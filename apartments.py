2a42380199c3ed970d998f39387b44c2252bc84f
Grant Dong <grant.dong@datadoghq.com>
2023-05-24 19:46:39 -0400

    improve code

--- apartments.py
+++ apartments.py
@@ -4,19 +4,20 @@ import pandas as pd
 from datetime import datetime
 
 HEADERS = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15",
-        "Accept": "test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-        "Accept-Language": "en-gb",
-        "Accept-Encoding": "br, gzip, deflate",
-        "Connection": "keep-alive",
-        "Upgrade-Insecure-Requests": "1",
-        "Sec-Fetch-Dest": "document",
-        "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "none",
-        "Sec-Fetch-User": "?1",
-        "Cache-Control": "max-age=0",
-        "Referer": "http://www.google.com/",
-    }
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15",
+    "Accept": "test/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-gb",
+    "Accept-Encoding": "br, gzip, deflate",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Cache-Control": "max-age=0",
+    "Referer": "http://www.google.com/",
+}
+
 
 class Apartments:
 
@@ -25,27 +26,43 @@ class Apartments:
         self.state = state
         self.beds = beds
         self.baths = baths
-        self.url = "https://www.apartments.com/" + '-'.join(self.city.lower().split(' ')) + '-' + self.state.lower() + '/'
+        self.url = self.build_url()
         self.data = {}
         self.date = datetime.now().strftime("%m-%d-%Y")
-    
+
+    def build_url(self):
+        return "https://www.apartments.com/" + '-'.join(self.city.lower().split(' ')) + '-' + self.state.lower() + '/'
+
     def print_query(self):
         print("Location of Interest:", self.city + ", " + self.state)
         print("Number of Beds:", self.beds)
         print("Number of Baths:", self.baths)
 
+    def fetch_html(self, url):
+        try:
+            response = requests.get(url, headers=HEADERS)
+            response.raise_for_status()
+            return BeautifulSoup(response.content, 'html.parser')
+        except requests.exceptions.HTTPError as errh:
+            print("Http Error:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            print("Error Connecting:", errc)
+        except requests.exceptions.Timeout as errt:
+            print("Timeout Error:", errt)
+        except requests.exceptions.RequestException as err:
+            print("Something went wrong", err)
+
     def get_listings(self):
-        main_page = requests.get(self.url, headers=HEADERS)
-        print(main_page) # if not a 200 response, need to update headers
-        main_page_html = BeautifulSoup(main_page.content, 'html.parser')
+        main_page_html = self.fetch_html(self.url)
+        if main_page_html is None:
+            return None
         page_range = main_page_html.find('span', class_="pageRange").text.split(' ')[-1]
         listings = []
         for i in range(1, int(page_range) + 1):
-            page = requests.get(self.url + str(i) + "/", headers=HEADERS)
-            page_html = BeautifulSoup(page.content, 'html.parser')
+            page_html = self.fetch_html(self.url + str(i) + "/")
             listings += page_html.find_all('li', class_="mortar-wrapper")
         return listings
-    
+
     def get_address(self, listing):
         element1 = listing.find('div', class_="property-address js-url")
         element2 = listing.find('p', class_="property-address js-url")
@@ -67,14 +84,18 @@ class Apartments:
         return "Call for Rent"
 
     def execute(self):
-        listings = search.get_listings()
-        self.data["address"] = [search.get_address(listing) for listing in listings]
-        self.data["price"] = [search.get_prices(listing) for listing in listings]
+        listings = self.get_listings()
+        if listings is None:
+            print("Oops, this place doesn't exist or has no results.")
+            return
+        self.data["address"] = [self.get_address(listing) for listing in listings]
+        self.data["price"] = [self.get_prices(listing) for listing in listings]
         pd.DataFrame(self.data).to_csv(self.city.replace(' ', '_')
-            + "_" + self.state
-            + "_" + self.date
-            + ".csv", index = False)
+                                       + "_" + self.state
+                                       + "_" + self.date
+                                       + ".csv", index=False)
 
-search = Apartments("Manhattan", 'NY', 2, 1)
+
+search = Apartments("Missoula", 'MT', 4, 2)
 search.print_query()
 search.execute()
